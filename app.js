ccca131c0f1daa6417d76d830f38c69744e143d4
Ben Chou <ben7152000@gmail.com>
2021-04-01 14:33:35 +0800

    finish 01

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 
 const PORT = 3000
 
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+
+require('./routes')(app)
+
 app.listen(PORT, () => {
   console.log(`The server is running on localhost:${PORT}`)
 })
+
+module.exports = app
