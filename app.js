ccd8486bd894f22829a3cd3edb2c7707fbaf7bad
Ben Chou <ben7152000@gmail.com>
2021-04-01 15:36:06 +0800

    finish 08

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 
 const PORT = 3000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 require('./routes')(app)
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userController')
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
@@ -10,5 +11,8 @@ router.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
 router.get('/restaurants', restController.getRestaurants)
 
 router.get('/admin/restaurants', adminController.getRestaurants)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 module.exports = router
