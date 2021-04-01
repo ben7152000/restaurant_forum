b5d4d7485ba48645efad33689cc3fd5b09358feb
Ben Chou <ben7152000@gmail.com>
2021-04-01 14:49:31 +0800

    finish 02

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
 
-router.get('/', (req, res) => res.redirect('restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
 
 router.get('/restaurants', restController.getRestaurants)
+
+router.get('/admin/restaurants', adminController.getRestaurants)
 
 module.exports = router
